68625bec69c6a27a13962a1b3c0de64328a2d5ac
unknown <achref.aguel@esprit.tn>
2023-05-18 18:29:17 +0200

    firstAdd

---
+++ src/components/banner/Media.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiAngular, SiMongodb, SiNodedotjs } from "react-icons/si";
+import { Link } from 'react-router-dom';
+const Media = () => {
+  return (
+    <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaTwitter />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNodedotjs />
+            </span>
+            <span className="bannerIcon">
+              <SiAngular />
+            </span>
+            <span className="bannerIcon">
+              <SiMongodb />
+            </span>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Media
---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    des: "Develop, test, improve web apps and Ensure the quality and functionality of apps is in line with your clients' requirements",
+  },
+
+ 
+  {
+    id: 6,
+    icon: <FaGlobe />,
+    title: "jci president 2023",
+    des: "Within the JCI, everyone has the opportunity to train, to discover new skills and above all, everyone has the right to make mistakes. We want our members to become better people and better citizens.",
+  },
+];
